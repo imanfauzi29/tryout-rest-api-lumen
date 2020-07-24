9c8a3e1acdec4e151af68b6a0e40d3b9aa84f3ca
Iman Fauzi <imanfauzi29@gmail.com>
2020-07-24 10:42:48 +0700

    fix

--- database/migrations/2020_07_24_022128_create_products_table.php
+++ database/migrations/2020_07_24_022128_create_products_table.php
@@ -16,7 +16,7 @@ class CreateProductsTable extends Migration
         Schema::create('products', function (Blueprint $table) {
             $table->id();
             $table->string("name");
-            $table->int("price");
+            $table->integer("price");
             $table->timestamps();
         });
     }
--- routes/web.php
+++ routes/web.php
@@ -21,5 +21,26 @@ $router->group(["prefix" => "api/v1"], function () use ($router) {
     $router->post("customer", "CustomerController@create");
     $router->get("customer/{id}", "CustomerController@getById");
     $router->delete("customer/{id}", "CustomerController@delete");
-    $router->patch("customer/{id}", "CustomerController@patch");
+    $router->put("customer/{id}", "CustomerController@update");
+
+    // Order
+    $router->get("order", "OrderController@get");
+    $router->post("order", "OrderController@create");
+    $router->get("order/{id}", "OrderController@getById");
+    $router->delete("order/{id}", "OrderController@delete");
+    $router->put("order/{id}", "OrderController@update");
+
+    // Product
+    $router->get("product", "ProductController@get");
+    $router->post("product", "ProductController@create");
+    $router->get("product/{id}", "ProductController@getById");
+    $router->delete("product/{id}", "ProductController@delete");
+    $router->put("product/{id}", "ProductController@update");
+
+    // Payment
+    $router->get("payment", "PaymentController@get");
+    $router->post("payment", "PaymentController@create");
+    $router->get("payment/{id}", "PaymentController@getById");
+    $router->delete("payment/{id}", "PaymentController@delete");
+    $router->put("payment/{id}", "PaymentController@update");
 });
