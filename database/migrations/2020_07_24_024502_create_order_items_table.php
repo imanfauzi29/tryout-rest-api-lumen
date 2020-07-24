a0fdf5b99895da0fd9f2cd1808636a84610f2f0e
Iman Fauzi <imanfauzi29@gmail.com>
2020-07-24 11:19:04 +0700

    order_item edit

--- database/migrations/2020_07_24_024502_create_order_items_table.php
+++ database/migrations/2020_07_24_024502_create_order_items_table.php
@@ -17,6 +17,7 @@ class CreateOrderItemsTable extends Migration
             $table->id();
             $table->integer("order_id")->unsigned();
             $table->integer("product_id")->unsigned();
+            $table->integer("quantity");
             $table->timestamps();
         });
     }
